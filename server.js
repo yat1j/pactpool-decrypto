b774632f6bfe7103134675331ec07218030de4ad
Yatin Arora <yatinarora156@gmail.com>
2026-04-29 12:23:41 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(__dirname));
+
+// Default route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
